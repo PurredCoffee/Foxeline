029b8b16d57597f4b6bf39ce39b54f9d9300c6e7
SnipUndercover <snipundercover@gmail.com>
2024-10-14 00:40:32 +0200

    Add line breaks for readability

--- Source/FoxelineModuleSettings.cs
+++ Source/FoxelineModuleSettings.cs
@@ -7,15 +7,19 @@ namespace Celeste.Mod.Foxeline
         Flat = 2,
         Unlit = 3
     }
+
     public class FoxelineModuleSettings : EverestModuleSettings
     {
         public TailVariant Tail { get; set; } = TailVariant.Furry;
+
         [SettingRange(0, 100, true)]
         public int TailBrushTint { get; set; } = 15;
 
         [SettingRange(25, 1000, true)]
         public int TailScale { get; set; } = 100;
+
         public bool FeatherTail { get; set; } = true;
+
         public bool PaintBrushTail { get; set; } = false;
 
         [SettingSubHeader("Bangs")]
@@ -39,17 +43,23 @@ namespace Celeste.Mod.Foxeline
         public class TailDefaults
         {
             public TailVariant Tail { get; set; } = TailVariant.None;
+
             [SettingRange(0, 100, true)]
             public int TailBrushTint { get; set; } = 15;
-            // limited to 255 because it's cast to byte when sent in cnet
+
+            //limited to 255 because it's cast to byte when sent in cnet
             [SettingRange(25, 255, true)]
             public int TailScale { get; set; } = 100;
+
             public bool FeatherTail { get; set; } = true;
+
             public bool PaintBrushTail { get; set; } = false;
         }
+
         [SettingSubMenu]
         public class BadelineTailDefaults : TailDefaults {
             public new TailVariant Tail { get; set; } = TailVariant.Flat;
+
             public bool EnableBangs { get; set; } = true;
         }
 
@@ -58,18 +68,21 @@ namespace Celeste.Mod.Foxeline
         {
             [SettingRange(0, 100, true)]
             public int droopSwayAmplitude { get; set; } = 20;
+
             [SettingRange(0, 100, true)]
             public int droopSwayFrequency { get; set; } = 20;
+
             [SettingRange(1, 100, true)]
             public int droopSwaySpeed { get; set; } = 30;
+
             [SettingRange(0, 100, true)]
             public int Control { get; set; } = 60;
+
             [SettingRange(0, 99, true)]
             public int Speed { get; set; } = 90;
+
             [SettingRange(0, 100, true)]
             public int Softness { get; set; } = 30;
-
-
         }
     }
 }
